6065947a748b84611e25a967347fd00c0b55369d
yungRicardo <rickyjespinoza@gmail.com>
2023-05-31 16:39:41 -0700

    blocks challenges

---
+++ blocks-ricky-jesus.rb
@@ -0,0 +1,47 @@
+# num = 1
+# while num <= 20
+#     p num
+#     num += 1
+# end
+
+# arr = 1..20
+# arr.each do |value|
+#     p value
+# end
+
+
+# num = 20
+# while num >= 0
+#     p num
+#     num-=2
+    
+# end
+
+# arr = [1, 2, 3]
+# arr2 = [4, 5, 6]
+
+# def times_five(arr)
+#     arr.each do |value|
+#         p value * 5
+#     end
+# end
+
+# timesFive(arr)
+# timesFive(arr2)
+
+# arr = ['cat', 'dog', 'bat']
+# def cap_words(arr)
+#     arr.map! do |value|
+#         p value.upcase
+#     end
+# end
+
+# cap_words(arr)
+
+nums = [10, 20, 50, 30, 40]
+puts nums.max()
+
+# def largest_num(bot)
+#     bot.select {|value| value}
+    
+

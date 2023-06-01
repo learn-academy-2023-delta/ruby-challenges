c402386940bf28024e6e3d88776dc2fae84c4a08
Phalco1 <elphalco1@gmail.com>
2023-06-01 18:40:17 -0500

    harder than it looked

---
+++ classes-paul-jesus.rb
@@ -0,0 +1,49 @@
+# As a developer, I can create a class called Task.
+
+# class Task 
+# end
+# p Task.new
+
+# As a developer, I can create three instances (objects) of class Task. e.g laundry = Task.new
+
+class Task 
+
+
+    def set_task(task)
+        @task = task
+    end
+
+    def set_task(task)
+        @task = task
+    end
+
+    def set_task(task)
+        @task = task
+    end
+
+    
+end
+
+laundry = Task.new
+p Task.new
+
+# As a developer, I can initialize each instance of class Task with a title.
+
+# def initialize(task)
+#     @task = name
+# end
+
+
+# As a developer, I can see the title of each instance of class Task.
+# As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
+# As a developer, I can update the status of each instance of class Task when the task has been completed.
+# For the following ColorPalette challenge use initialize and attr_accessor methods in your class.
+
+# As a developer, I can create a class called ColorPalette.
+# As a developer, I can create three instances (objects) of class ColorPalette.
+# e.g green = ColorPalette.new
+# As a developer, I can initialize each instance of the class ColorPalette with three colors.
+# e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+# As a developer, I can print the value of each individual color.
+# As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
+# As a developer, I can change one or more colors of a given palette.

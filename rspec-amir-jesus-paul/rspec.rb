45aa71486968ee6689ab94951f7515be0e6f6ae8
Phalco1 <elphalco1@gmail.com>
2023-06-06 18:45:11 -0500

    dishes, dishes, dishes

--- rspec-amir-jesus-paul/rspec.rb
+++ rspec-amir-jesus-paul/rspec.rb
@@ -8,26 +8,53 @@
 # As a developer, I can give a Task a title and retrieve it.
 
 
- class Task
-    attr_accessor :title 
-    def initialize(title)
-        @title = title
-    end 
- end
+#  class Task
+#     attr_accessor :title 
+#     def initialize(title)
+#         @title = title
+#     end 
+#  end
 
 
 
 # As a developer, I can give a Task a description and retrieve it.
 
+# class Task
+#     attr_accessor :title, :description
 
+#     def initialize(title, description='wash, rinse, repeat')
+#         @title = title
+#         @description = description
+#     end 
+#  end
 
 # As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
 
-
+# class Task
+#         attr_accessor :title, :description, :status 
+    
+#         def initialize(title, description='wash, rinse, repeat', status= 'In Progress')
+#             @title = title
+#             @description = description
+#             @status = status
+#         end 
+#      end
 
 # As a developer, when I print a Task that is done, its status is shown.
 
+class Task
+        attr_accessor :title, :description, :status 
+    
+        def initialize(title, description='wash, rinse, repeat', status= 'In Progress')
+            @title = title
+            @description = description
+            @status = status
+        end 
 
+        def update execution
+            @status += execution
+        end
+     end
 
 
 # As a developer, I can give a Task a due date. Hint: Use the built-in Ruby Date class.
--- rspec-amir-jesus-paul/rspec_spec.rb
+++ rspec-amir-jesus-paul/rspec_spec.rb
@@ -3,11 +3,30 @@ require_relative 'rspec'
 
 describe Task do
     it 'has to be real' do
-        expect{ Task.new('Harry Pothead') }.to_not raise_error
+        expect{ Task.new('Wash the dishes') }.to_not raise_error
     end
     it 'has a title' do
-        task = Task.new('Harry Pothead')
-        expect(task.title).to eq ('Harry Pothead')
+        task = Task.new('Wash the dishes')
+        expect(task.title).to eq ('Wash the dishes')
         expect(task.title).to be_a(String)
     end
+    it 'give the task a description' do
+        task = Task.new('Wash the dishes')
+        expect(task.description). to eq('wash, rinse, repeat')
+        expect(task.description).to be_a(String)
+        clean_dishes = Task.new('wash the dishes', 'clean')
+        expect(clean_dishes.description).to eq('clean')
+    end
+    it 'can check the status' do
+        task = Task.new('Wash the dishes')
+        expect(task.status).to eq('In Progress')
+        expect(task.status).to be_a(String)
+    end
+    it 'can update and show the status' do
+    task = Task.new('Wash the dishes')
+    expect{ task.update('Done') }.to change{ task.status }.from('In Progress').to('Done')
+    end
 end
+
+# Failure/Error: expect{ task.update('Done') }.to change{ task.status }.from('In Progress').to('Done')
+#        expected `task.status` to have changed to "Done", but is now "In ProgressDone"

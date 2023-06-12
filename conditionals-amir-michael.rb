5630df44231ad512de433f4d15da662a0f231951
immikeoa <immikeoa@gmail.com>
2023-06-12 19:58:53 -0400

    sorrry.

---
+++ conditionals-amir-michael.rb
@@ -0,0 +1,28 @@
+# Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+
+def sum_these_numbers(inter1, inter2)
+    inter1 + inter2
+    end
+    
+     p sum_these_numbers(5,8) #13
+    
+    # Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+    def  is_even(inter)
+        if inter % 2 == 0
+            'Is even'
+        else
+            'Is odd'
+        
+        end
+    
+    end
+    
+    p is_even(2)
+    
+    # Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+    def  is_valid
+    
+    end
+    
+    # Create a method that takes in a string and determines if the string is a 
+    # palindrome.
